e11dfb8d85a5485ae658fa819b39422cc08f48e9
Pasindu Gunawardana <pgun8312.sysco@com>
2023-12-01 19:47:45 +0530

    Addressed a bug

--- product-service/src/test/java/com/fos/api/controller/ProductControllerTest.java
+++
@@ -1,8 +0,0 @@
-package com.fos.api.controller;
-
-import com.fos.api.ProductServiceApplicationTests;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class ProductControllerTest{
-}
